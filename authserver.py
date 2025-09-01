bda98169aa34ccb117e43717481afe6979133c25
A3377 <71415034+A3377@users.noreply.github.com>
2025-08-31 21:47:37 -0600

    Update authserver.py

--- authserver.py
+++ authserver.py
@@ -72,4 +72,6 @@ def challenge():
     return jsonify({"status": "success"})
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
